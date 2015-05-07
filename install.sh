3950010d653716471ef2033ed69bdd633508b2fe
Keynoru <e@xtendo.org>
2015-05-07 23:07:57 +0900

    make install.sh autopilot

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-sudo apt-get install git vim tree curl silversearcher-ag
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install git vim tree curl silversearcher-ag
 git clone --depth=1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/clefairy/.tmux.conf ~/
 ln -s ~/clefairy/.vimrc ~/
