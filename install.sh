394f3793a44f6da777c23ac194e47d4534e55d72
Keynoru <e@xtendo.org>
2015-05-20 19:32:14 +0900

    change name to ... (klutzy enlightened me)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install git vim tree curl silversearcher-ag
-git clone --depth=1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim & git clone --depth=1 https://github.com/keynoru/clefairy.git ~/clefairy
-ln -s ~/clefairy/.tmux.conf ~/
-ln -s ~/clefairy/.vimrc ~/
+git clone --depth=1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim & git clone --depth=1 https://github.com/keynoru/....git ~/...
+ln -s ~/.../.tmux.conf ~/
+ln -s ~/.../.vimrc ~/
