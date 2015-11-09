d836add7e2fb96ed640599c3ac763f9cb9a1270f
kinoru <xkinoru@gmail.com>
2015-11-09 15:52:42 +0900

    Add compiz config file
    desktop.sh is now idempotent!

--- desktop.sh
+++ desktop.sh
@@ -1,4 +1,5 @@
-sudo apt-get install -y dzen2 compizconfig-settings-manager
-sudo ln -s desktop/compiz.desktop /usr/share/xsessions/
-sudo ln -s desktop/compiz-session /usr/local/bin/
-sudo chmod 755 /usr/local/bin/compiz-session
+sudo bash -c "apt-get install -y dzen2 compizconfig-settings-manager;
+sudo ln -sf ~/.../desktop/compiz.desktop /usr/share/xsessions/;
+sudo ln -sf ~/.../desktop/compiz-session /usr/local/bin/;
+sudo chmod 755 /usr/local/bin/compiz-session"
+ln -sf ~/.../desktop/Default.ini ~/.config/compiz-1/compizconfig/
