3d0c4abbe04fd95108db6f3e60287a02f7e672ee
kinoru <xkinoru@gmail.com>
2015-11-09 16:28:58 +0900

    install.sh is now idempotent

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install git vim tree curl silversearcher-ag fish
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s ~/.../.tmux.conf ~/
-ln -s ~/.../.vimrc ~/
+sudo bash -c "apt-get update &&
+apt-get -y upgrade &&
+apt-get -y install git vim tree curl silversearcher-ag fish wget openssh-server ca-certificates sudo tmux"
+[ -f ~/.vim/autoload/plug.vim ] || \
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -sf ~/.../.tmux.conf ~/
+ln -sf ~/.../.vimrc ~/
 mkdir -p ~/.config/fish
-ln -s ~/.../config.fish ~/.config/fish/config.fish
+ln -sf ~/.../config.fish ~/.config/fish/config.fish
