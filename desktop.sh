56732d94ccdf8d1d46d3831d64743e5d70df1a27
kinoru <xkinoru@gmail.com>
2015-11-13 20:10:54 +0900

    desktop.sh: remove unnecessary sudo in root

--- desktop.sh
+++ desktop.sh
@@ -1,5 +1,5 @@
 sudo bash -c "apt-get install -y dzen2 compizconfig-settings-manager;
-sudo ln -sf ~/.../desktop/compiz.desktop /usr/share/xsessions/;
-sudo ln -sf ~/.../desktop/compiz-session /usr/local/bin/;
-sudo chmod 755 /usr/local/bin/compiz-session"
+ln -sf ~/.../desktop/compiz.desktop /usr/share/xsessions/;
+ln -sf ~/.../desktop/compiz-session /usr/local/bin/;
+chmod 755 /usr/local/bin/compiz-session"
 ln -sf ~/.../desktop/Default.ini ~/.config/compiz-1/compizconfig/
