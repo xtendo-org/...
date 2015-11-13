01c2ce233e5798014e11336bab5d5d68eb07195f
kinoru <xkinoru@gmail.com>
2015-11-13 09:03:45 +0000

    install.sh: garbage collect temporary file

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo $USER > /tmp/currentusername
 sudo bash -c "apt-get -y install git vim tree curl silversearcher-ag fish wget openssh-server ca-certificates sudo tmux && \
     cat /tmp/currentusername | xargs chsh -s /usr/bin/fish"
+rm /tmp/currentusername
 [ -f ~/.vim/autoload/plug.vim ] || \
     (curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
