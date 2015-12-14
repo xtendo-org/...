49b5b9e816618c3a37a6f50f5648d7da5e5d4629
kinoru <xkinoru@gmail.com>
2015-12-14 17:52:46 +0900

    .fonts.conf: fix wrong file linkage

--- fonts.sh
+++ fonts.sh
@@ -6,5 +6,6 @@ cd ~/.fonts
 wget https://gist.github.com/kinoru/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520for%2520Powerline.ttf https://gist.github.com/kinoru/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520Bold%2520for%2520Powerline.ttf https://gist.github.com/kinoru/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520Italic%2520for%2520Powerline.ttf
 wget http://www.latofonts.com/download/Lato2OFL.zip
 unzip Lato2OFL.zip
-ln -sf ~/.../.fonts.conf ~/
+ln -sf ~/.../.fonts.conf ~/.config/fontconfig/fonts.conf
+ln -sf ~/.config/fontconfig/fonts.conf ~/.fonts.conf
 fc-cache -v
