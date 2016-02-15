9d67e91055cd2fe37a81076db4172f4ffe084b74
kinoru <xkinoru@gmail.com>
2016-02-15 11:02:24 +0900

    install.sh: annotate

--- install.sh
+++ install.sh
@@ -1,21 +1,27 @@
+set -e # stop immediately if anything fails
+
 # echo $USER > /tmp/currentusername
 # sudo bash -c "apt-get -y install git vim tree curl silversearcher-ag fish wget openssh-server ca-certificates sudo tmux && \
 #     cat /tmp/currentusername | xargs chsh -s /usr/bin/fish"
 # rm /tmp/currentusername
+
+echo "Configuring Vim..."
 mkdir -p ~/tmp
 ln -sf ~/.../.vimrc ~/
 [ -f ~/.vim/autoload/plug.vim ] || \
     (curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
     vim -c "PlugUpdate | qa")
+echo "Configuring Tmux..."
 ln -sf ~/.../.tmux.conf ~/
+echo "Configuring bash..."
 grep -Fxq "source ~/.../.bashrc" ~/.bashrc || \
     echo "source ~/.../.bashrc" >> ~/.bashrc
 
-# fish
+echo "Configuring fish..."
 mkdir -p ~/.config/fish/config/fish
 ln -sf ~/.../config.fish ~/.config/fish/config.fish
 
-# git
+echo "Configuring Git..."
 ln -sf ~/.../gitconfig ~/.gitconfig
 ln -sf ~/.../gitexclude ~/.gitexclude
