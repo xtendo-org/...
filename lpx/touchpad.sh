72d0078b1cb9914a30f60373713679e1fbb32e51
kinoru <xkinoru@gmail.com>
2016-03-24 19:54:26 +0900

    touchpad.sh: enable LED

--- lpx/touchpad.sh
+++ lpx/touchpad.sh
@@ -1,1 +1,4 @@
-(xinput list-props 13 | awk '/Device Enabled/ {print $4}' | xargs test 1 ==) && xinput set-prop 13 "Device Enabled" 0 || xinput set-prop 13 "Device Enabled" 1
+#!/usr/bin/env bash
+(xinput list-props 13 | awk '/Device Enabled/ {print $4}' | xargs test 1 ==) && export TOUCHPAD=0 || export TOUCHPAD=1
+xinput set-prop 13 "Device Enabled" $TOUCHPAD
+echo $TOUCHPAD | sudo tee "/sys/class/leds/input0::capslock/brightness" > /dev/null
