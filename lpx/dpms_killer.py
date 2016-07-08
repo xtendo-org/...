5fa6277abb16eae020c69df72a02fe6786acb4c2
kinoru <xkinoru@gmail.com>
2016-07-08 16:53:44 +0900

    X: improve DPMS killer

--- lpx/dpms_killer.py
+++ lpx/dpms_killer.py
@@ -3,12 +3,16 @@
 import datetime
 import subprocess
 
+def dpms_off():
+    subprocess.run(['xset', '-dpms'], check=False)
+    subprocess.run(['xset', 's', 'off'], check=False)
+    subprocess.run(['xset', 's', 'off', '-dpms'], check=False)
+    subprocess.run(['bash', '-c', 'echo "DPMS off" | dzen2 -title-name "job finished" -x 10 -y 10 -w 200 -h 50 -p 2 -bg orange -fg black -fn Lato-10'])
+
+dpms_off()
+
 acpi_listen = subprocess.Popen(['acpi_listen'], stdout=subprocess.PIPE)
 while True:
     line = acpi_listen.stdout.readline().decode('ascii')
-    print('[%s]' % datetime.datetime.now().isoformat(), line, end='')
     if line.startswith('ac_adapter '):
-        print('Turning DPMS off...')
-        subprocess.run(['xset', '-dpms'], check=False)
-        subprocess.run(['xset', 's', 'off'], check=False)
-        subprocess.run(['xset', 's', 'off', '-dpms'], check=False)
+        dpms_off()
