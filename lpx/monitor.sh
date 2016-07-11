42f189e45a98c2a0aafabd541320a3c97dd8c48c
kinoru <xkinoru@gmail.com>
2016-07-11 09:52:45 +0900

    script: monitor.sh to detect DPI

--- lpx/monitor.sh
+++ lpx/monitor.sh
@@ -1,1 +1,9 @@
-xrandr | grep -q "^HDMI1 connected" && (xrandr --output HDMI1 --auto && xrandr --output eDP1 --off) || (xrandr --output eDP1 --mode 1920x1080 --scale 2x2 --panning 3840x2160)
+if xrandr | grep -q "^HDMI1 connected"; then
+    xrandr --output HDMI1 --auto && xrandr --output eDP1 --off
+else
+    if grep -q 'Xft\.dpi: 192' ~/.Xresources; then
+        xrandr --output eDP1 --mode 1920x1080 --scale 2x2 --panning 3840x2160
+    else
+        xrandr --output eDP1 --auto
+    fi
+fi
