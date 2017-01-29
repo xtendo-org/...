1b11aee643f779ba0fe26fed7f03617dd402fce4
XT <e@xtendo.org>
2017-01-29 20:38:12 +0900

    fish: Extract tartar as a plugin

--- config.fish
+++ config.fish
@@ -13,78 +13,10 @@ function prompt_long_pwd --description 'Print the current working directory'
     end
 end
 
-# tartar begins
+# tartar configuration
 
-set -g tartar_bg NONE
-
-function prompt_open
-    echo -n (set_color -b $argv[1])(set_color $argv[2]) ''
-    set tartar_bg $argv[1]
-end
-
-function prompt_transition
-    # argv 1: new background color
-    # argv 2: new foreground color
-    echo -n '' (set_color -b $argv[1])(set_color $tartar_bg)\uE0B0(set_color $argv[2]) ''
-    set tartar_bg $argv[1]
-end
-
-function prompt_close
-    echo -n '' (set_color -b normal)(set_color $tartar_bg)\uE0B0(set_color normal) ''
-end
-
-function fish_prompt
-    # hostname
-    prompt_open blue white
-    echo -n (hostname)
-
-    # path
-    prompt_transition black white
-    echo -n (prompt_long_pwd)
-
-    # pyenv
-    if [ $PYENV_VIRTUAL_ENV ];
-        prompt_transition magenta white
-        echo -n (basename $PYENV_VIRTUAL_ENV);
-    end
-
-    # git
-    set -l git_dir (git rev-parse --git-dir 2> /dev/null)
-    if test -n "$git_dir"
-        set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
-        if test "$branch" = 'master'
-            set branch ''
-        end
-        set -l git_status (git status -s 2> /dev/null | grep -v "^??")
-        if test -n "$git_status"
-            set git_color yellow
-        else
-            set git_color green
-        end
-        set -l git_ahead (git rev-list origin/master.. 2> /dev/null | wc -l | tr -d '[:space:]')
-        if git status -s 2> /dev/null | grep -q "^??"
-            set git_untracked "+"
-        end
-        if [ "$git_ahead" != 0 ]
-            set git_str " $git_ahead"
-        end
-        set git_stash (git stash list | wc -l)
-        if [ $git_stash = 0 ]
-            set git_stash
-        end
-        prompt_transition $git_color white
-        echo -n $git_stash $branch $git_untracked $git_str
-    end
-
-    # current jobs
-    set -l current_jobs (jobs | grep -v /usr/bin/fish | wc -l | tr -d '[:space:]')
-    if [ "$current_jobs" != 0 ]
-        prompt_transition red white
-        echo -n $current_jobs
-    end
-
-    prompt_close
-end
+set TARTAR_PATH_BG 9cf
+set TARTAR_PATH_FG black
 
 function hunt
     ps aux | grep $argv | grep -v grep | awk '{print $2}'
@@ -122,13 +54,10 @@ if [ -d ~/.rbenv ]
     . (rbenv init -|psub)
 end
 
-# chips: the fish plugin manager. https://github.com/kinoru/chips
-if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
-alias chipsedit "v ~/.config/chips/plugin.yaml"
-
 # aliases
 alias u "unbreak open"
 alias v "vim --servername (random)"
+alias vs "vim --servername (random) -S"
 
 # Directory-based pyenv-virtualenv
 function _pyenv-virtualenv
@@ -160,3 +89,7 @@ _pyenv-virtualenv
 # Change Ctrl+w to delete up to the last whitespace;
 # use Alt+Backspace for deleting path components.
 bind \cw backward-kill-bigword
+
+# chips
+if [ -e ~/.config/chips/build.fish ] ; . ~/.config/chips/build.fish ; end
+alias chipsedit "v ~/.config/chips/plugin.yaml"
