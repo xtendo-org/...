bf223bac3de4d8fba1ecf567e22ac12b22c9fa23
XT <e@xtendo.org>
2017-10-23 18:30:04 +0900

    autosync

--- lpx/configure_touchpad.sh
+++ lpx/configure_touchpad.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 # man synaptics
 # http://www.x.org/archive/X11R7.5/doc/man/man4/synaptics.4.html
-synclient PalmDetect=1
-synclient PalmMinWidth=8
+synclient PalmDetect=0
+synclient PalmMinWidth=10
 synclient PalmMinZ=1
 
-synclient PressureMotionMinZ=15
+synclient PressureMotionMinZ=4
 synclient EmulateTwoFingerMinZ=80
 synclient EmulateTwoFingerMinW=1
 
-synclient FingerLow=5
-synclient FingerHigh=15
+synclient FingerLow=1
+synclient FingerHigh=1
 
-synclient AccelFactor=0.15
-synclient MinSpeed=0.5
-synclient MaxSpeed=6
+synclient AccelFactor=0.1
+synclient MinSpeed=0.1
+synclient MaxSpeed=7
 
-synclient MaxTapTime=100
-synclient MaxTapMove=1
+synclient MaxTapTime=50
+synclient MaxTapMove=60
 synclient SingleTapTimeout=150
 synclient MaxDoubleTapTime=50
 
