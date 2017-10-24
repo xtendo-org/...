eb65e618d72772eeb7c8ae47b27ed96a0b8faf98
XT <e@xtendo.org>
2017-10-25 00:49:28 +0900

    autosync

--- fonts.sh
+++ fonts.sh
@@ -2,8 +2,8 @@
 set -e
 # sudo apt-get install unzip
 cd ~/.local/share/fonts
-[ -f Envy\%20Code\%20R\%20for\%20Powerline.ttf ] || wget https://gist.github.com/kinoru/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520for%2520Powerline.ttf https://gist.github.com/kinoru/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520Bold%2520for%2520Powerline.ttf https://gist.github.com/kinoru/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520Italic%2520for%2520Powerline.ttf
+[ -f Envy\%20Code\%20R\%20for\%20Powerline.ttf ] || wget https://gist.github.com/xtendo-org/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520for%2520Powerline.ttf https://gist.github.com/xtendo-org/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520Bold%2520for%2520Powerline.ttf https://gist.github.com/xtendo-org/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520Italic%2520for%2520Powerline.ttf
 [ -f Lato2OFL.zip ] || (wget http://www.latofonts.com/download/Lato2OFL.zip && unzip Lato2OFL.zip)
-mkdir -p ~/.config/fontconfig
-ln -sf ~/.../.fonts.conf ~/.config/fontconfig/fonts.conf
+# mkdir -p ~/.config/fontconfig
+# ln -sf ~/.../.fonts.conf ~/.config/fontconfig/fonts.conf
 fc-cache -v
