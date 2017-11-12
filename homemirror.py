2e9c42a5ed556b37a3e57a2822517443bf305968
XT <e@xtendo.org>
2017-11-12 16:28:39 +0900

    autosync

--- homemirror.py
+++ homemirror.py
@@ -11,6 +11,8 @@ if not targets[-1]:
 
 for target in targets[1:]:
     if os.path.isfile(target):
+        print('Linking: ',  target)
         subprocess.run(['ln', '-sf', target, HOME + target[len(mirror_dir):]])
     else:
         print(target, 'is directory')
+        os.makedirs(HOME + target[len(mirror_dir):], exist_ok=True)
