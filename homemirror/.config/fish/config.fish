319f95632b3470a0f4a61e6e500355de1dd6149d
XT <e@xtendo.org>
2018-08-20 17:09:10 +0900

    autosync

--- homemirror/.config/fish/config.fish
+++ homemirror/.config/fish/config.fish
@@ -27,15 +27,6 @@ function pyenv
     command pyenv "$command" $argv
   end
 end
-function _pyenv_virtualenv_hook --on-event fish_prompt;
-  set -l ret $status
-  if [ -n "$VIRTUAL_ENV" ]
-    pyenv activate --quiet; or pyenv deactivate --quiet; or true
-  else
-    pyenv activate --quiet; or true
-  end
-  return $ret
-end
 
 # Directory-based pyenv-virtualenv
 function _pyenv-virtualenv
