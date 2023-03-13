fe43c4e712fcbb689a8187baa60b68d03aa8b6e3
XT <e@xtendo.org>
2023-03-13 13:58:22 +0900

    autosync

--- fonts.sh
+++ fonts.sh
@@ -3,7 +3,10 @@ set -e
 # sudo apt-get install unzip
 cd ~/.local/share/fonts
 [ -f Envy\%20Code\%20R\%20for\%20Powerline.ttf ] || wget https://gist.github.com/xtendo-org/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520for%2520Powerline.ttf https://gist.github.com/xtendo-org/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520Bold%2520for%2520Powerline.ttf https://gist.github.com/xtendo-org/17b35145d638ea4071d1/raw/e1208f47f9ef365bf29501f16245306620b1f29a/Envy%2520Code%2520R%2520Italic%2520for%2520Powerline.ttf
-[ -f Lato2OFL.zip ] || (wget http://www.latofonts.com/download/Lato2OFL.zip && unzip Lato2OFL.zip)
+
+## Lato is now available in repo
+# [ -f Lato2OFL.zip ] || (wget http://www.latofonts.com/download/Lato2OFL.zip && unzip Lato2OFL.zip)
+
 # mkdir -p ~/.config/fontconfig
 # ln -sf ~/.../.fonts.conf ~/.config/fontconfig/fonts.conf
 fc-cache -v
