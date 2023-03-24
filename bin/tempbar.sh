1c0e88083ac365b8e17c768cbf998ec8e90668b5
XT <e@xtendo.org>
2023-03-24 11:27:22 +0900

    autosync

--- bin/tempbar.sh
+++ bin/tempbar.sh
@@ -1,1 +1,10 @@
-while true; do echo '^fn(Lato-12)' $(date +'%Y-%m-%d (%a) %H:%M') · $(uptime -p) · $(ip --brief addr show | grep -v '^lo' | sed -e 's|\([0-9a-z]\{4,\}\)\s\+\([A-Z]\+\)\s\+\([.0-9]\+\)\?\+.*|\1 \2 \3|' | head -c -2 | tr '\n' '/' | sed -e 's|/| / |g') $(iwctl station wlan0 show | grep -m 1 'Connected' | sed -e 's/Connected network/· /') $(iwctl station wlan0 show | grep -m 1 RSSI | grep -o '\-\?[0-9]\+') '·' $(acpi | sed -e 's/Battery 0: //'); sleep 3; done | dzen2 -title-name up -y -1 -bg $( [ $(( $(date +%j) % 2 )) -eq 0 ] && echo black || echo white ) -fg $( [ $(( $(date +%j) % 2 )) -eq 0 ] && echo white || echo black ) -h 16
+while true; do
+    echo '^fn(Lato-12)' \
+      $(date +'%Y-%m-%d (%a) %H:%M') · \
+      $(uptime -p) · \
+      $(ip --brief addr show | grep '\s\+UP\s\+' | sed -e 's|\([0-9a-z]\{4,\}\)\s\+\([A-Z]\+\)\s\+\([.0-9]\+\)\?\+.*|\1 \3|' | head -c -1 | tr '\n' '/' | sed -e 's|/| / |g') \
+      $(iwctl station wlan0 show | grep -m 1 'Connected' | sed -e 's/Connected network/· /') \
+      $(iwctl station wlan0 show | grep -m 1 RSSI | grep -o '\-\?[0-9]\+') · \
+      $(acpi | sed -e 's/Battery 0: //');
+    sleep 3
+done | dzen2 -title-name up -y -1 -bg $( [ $(( $(date +%_j) % 2 )) -eq 0 ] && echo black || echo white ) -fg $( [ $(( $(date +%_j) % 2 )) -eq 0 ] && echo white || echo black ) -h 48
