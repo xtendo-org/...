f75e980988f7c4a09444700e4523c7761614ac4a
XT <git@xtendo.org>
2023-05-25 13:03:10 +0900

    autosync

--- lpx/init_monitor.sh
+++ lpx/init_monitor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if xrandr | grep -q "3840"; then
+if xrandr | rg -q "3840"; then
     sed -i "s/^Xft\.dpi: .*$/Xft.dpi: 192/g" ~/.Xresources
 else
     sed -i "s/^Xft\.dpi: .*$/Xft.dpi: 96/g" ~/.Xresources
---
+++ z13/monitor.sh
@@ -0,0 +1,9 @@
+if xrandr | rg -q "^DP-1 connected"; then
+    xrandr --output DP-1 --auto && xrandr --output eDP-1 --off
+else
+    if grep -q 'Xft\.dpi: 192' ~/.Xresources; then
+        xrandr --output eDP-1 --mode 1920x1080 --scale 2x2 --panning 3840x2160
+    else
+        xrandr --output eDP-1 --auto
+    fi
+fi
