2d0fc66cdd3103c41c5bf93d15b823e4fb44e81a
XT <git@xtendo.org>
2024-01-28 21:39:38 +0900

    fish_prompt.fish: trim git numbers

--- homemirror/.config/fish/functions/fish_prompt.fish
+++ homemirror/.config/fish/functions/fish_prompt.fish
@@ -5,18 +5,18 @@ function fish_prompt
   end
   echo \
     (set_color -b blue white)$CMD_DURATION(set_color normal)\
-    (set_color -o cyan)(hostname)':'(set_color normal)\
+    (set_color -o cyan)$FISH_HOSTNAME':'(set_color normal)\
     (if [ $PYENV_VERSION ]; echo (set_color -b blue brwhite)$PYENV_VERSION(set_color normal); end)\
     (if [ $CONDA_DEFAULT_ENV ]; echo (set_color -b green brwhite)$CONDA_DEFAULT_ENV(set_color normal); end)\
     (set_color -o blue)(pwd | sed -e "s|^$HOME/work/prex-||" -e "s|^$HOME/code/||" -e "s|^$HOME/work/||" -e "s|^$HOME/||" -e "s|^$HOME|~|")(set_color normal)\
     (if set -l tartar_gitrev (git rev-parse --is-inside-work-tree --abbrev-ref HEAD 2>/dev/null)
-      set -l tartar_untracked_count (git ls-files --others --exclude-standard | wc -l)
       git diff-index --quiet HEAD; and set -l tartar_gitbg green; or set -l tartar_gitbg yellow
       echo -n (set_color -b $tartar_gitbg white)$tartar_gitrev[2]
-      set -l tartar_stash_count (git stash list | wc -l)
+      set -l tartar_untracked_count (string trim (git ls-files --others --exclude-standard | wc -l))
       if [ $tartar_untracked_count != 0 ]
         echo -n '+'$tartar_untracked_count
       end
+      set -l tartar_stash_count (string trim (git stash list | wc -l))
       if [ $tartar_stash_count != 0 ]
         echo -n '#'$tartar_stash_count
       end
