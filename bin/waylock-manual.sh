a82f558567ff0064a5fd453e8337b4878b38aa98
XT <git@xtendo.org>
2026-08-14 19:26:18 +0900

    autosync

--- bin/waylock-manual.sh
+++ bin/waylock-manual.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-swayidle timeout 1 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' &
+swayidle timeout 1 "swaymsg output \* dpms off" resume "swaymsg output \* dpms on" &
 pid=$!
 echo "pid=$pid"
 pactl set-sink-mute @DEFAULT_SINK@ 1
--- scripts/waylock-init.sh
+++ scripts/waylock-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while ! swaymsg -t get_version > /dev/null 2>&1; do sleep 0.1; done
 swayidle -w \
-  timeout 600 'swaymsg output * dpms off' \
-  resume 'swaymsg output * dpms on' \
+  timeout 600 "swaymsg output \* dpms off" \
+  resume "swaymsg output \* dpms on" \
   timeout 1200 "waylock -fork-on-lock" \
   before-sleep "waylock -fork-on-lock"
